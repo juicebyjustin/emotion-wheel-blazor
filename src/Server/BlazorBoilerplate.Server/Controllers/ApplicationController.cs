bdee5e977f0af54d2b39a77ca14a60fd6ea5695d
Justin <2144123+juicebyjustin@users.noreply.github.com>
2021-04-20 09:34:52 -0500

    Initial commit

---
+++ src/Server/BlazorBoilerplate.Server/Controllers/ApplicationController.cs
@@ -0,0 +1,108 @@
+﻿using BlazorBoilerplate.Infrastructure.AuthorizationDefinitions;
+using BlazorBoilerplate.Infrastructure.Storage.DataModels;
+using BlazorBoilerplate.Storage;
+using Breeze.AspNetCore;
+using Breeze.Persistence;
+using Breeze.Persistence.EFCore;
+using IdentityServer4.AccessTokenValidation;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorBoilerplate.Server.Controllers
+{
+    [Route("api/data/[action]")]
+    [Authorize(AuthenticationSchemes = AuthSchemes)]
+    [BreezeQueryFilter]
+    public class ApplicationController : Controller
+    {
+        private const string AuthSchemes =
+            "Identity.Application" + "," + IdentityServerAuthenticationDefaults.AuthenticationScheme; //Cookie + Token authentication
+
+        private ApplicationPersistenceManager persistenceManager;
+        public ApplicationController(ApplicationPersistenceManager persistenceManager)
+        {
+            this.persistenceManager = persistenceManager;
+        }
+
+        [AllowAnonymous]
+        [HttpGet]
+        public string Metadata()
+        {
+            return persistenceManager.Metadata();
+        }
+
+        [HttpGet]
+        public Task<UserProfile> UserProfile()
+        {
+            return persistenceManager.GetUserProfile();
+        }
+
+        [AllowAnonymous]
+        [HttpGet]
+        public IQueryable<TenantSetting> TenantSettings()
+        {
+            return persistenceManager.GetEntities<TenantSetting>();
+        }
+
+        [HttpGet]
+        [Authorize(Policies.IsAdmin)]
+        public IQueryable<ApplicationUser> Users()
+        {
+            return persistenceManager.GetEntities<ApplicationUser>().Include(i => i.UserRoles).ThenInclude(i => i.Role).OrderBy(i => i.UserName);
+        }
+
+        [HttpGet]
+        [Authorize(Policies.IsAdmin)]
+        public IQueryable<ApplicationRole> Roles()
+        {
+            return persistenceManager.GetEntities<ApplicationRole>().OrderBy(i => i.Name);
+        }
+
+        [AllowAnonymous]
+        [HttpGet]
+        public IQueryable<Todo> Todos()
+        {
+            return persistenceManager.GetEntities<Todo>().Include(i => i.CreatedBy).Include(i => i.ModifiedBy).OrderBy(i => i.Id);
+        }
+
+        [HttpGet]
+        [Authorize(Policies.IsAdmin)]
+        public IQueryable<DbLog> Logs()
+        {
+            return persistenceManager.GetEntities<DbLog>().AsNoTracking().OrderByDescending(i => i.TimeStamp);
+        }
+
+        [HttpGet]
+        [Authorize(Policies.IsAdmin)]
+        public IQueryable<ApiLogItem> ApiLogs()
+        {
+            return persistenceManager.GetEntities<ApiLogItem>().AsNoTracking().OrderByDescending(i => i.RequestTime);
+        }
+
+        [AllowAnonymous]
+        [HttpPost]
+        public SaveResult SaveChanges([FromBody] JObject saveBundle)
+        {
+            try
+            {
+                return persistenceManager.SaveChanges(saveBundle);
+            }
+            catch (EntityErrorsException)
+            {
+                throw;
+            }
+            catch (Exception ex)
+            {
+                var errors = new List<EFEntityError>();
+                errors.Add(new EFEntityError(null, null, ex.GetBaseException().Message, null));
+                throw new EntityErrorsException(errors);
+            }
+        }
+    }
+}
---
+++ src/Shared/BlazorBoilerplate.Infrastructure/Server/IEmailManager.cs
@@ -0,0 +1,17 @@
+﻿using BlazorBoilerplate.Infrastructure.Server.Models;
+using BlazorBoilerplate.Shared.Dto.Email;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace BlazorBoilerplate.Infrastructure.Server
+{
+    public interface IEmailManager
+    {
+        Task<ApiResponse> SendTestEmail(EmailDto parameters);
+        Task<ApiResponse> Receive();
+        Task<ApiResponse> SendEmailAsync(EmailMessageDto emailMessage);
+        List<EmailMessageDto> ReceiveEmail(int maxCount = 10);
+        Task<ApiResponse> ReceiveMailImapAsync();
+        Task<ApiResponse> ReceiveMailPopAsync(int min = 0, int max = 0);
+    }
+}
